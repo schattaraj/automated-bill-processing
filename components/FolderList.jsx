a6073cd55f9a399117ddec24f543dfdd2f79af32
schattaraj <schattaraj200@gmail.com>
2024-09-24 22:46:47 +0530

    fixing build error

--- components/FolderList.jsx
+++ components/FolderList.jsx
@@ -5,8 +5,8 @@ const FolderList = ({ items }) => {
         <>
             <div className="folder-list">
                 {
-                    items.map((item) => {
-                        return <Link className="item" href={"/Folders"}>
+                    items.map((item,index) => {
+                        return <Link className="item" href={"/Folders"} key={`folderItem${index}`}>
                             <img src="/icons/folder.svg" alt="" />
                             <span>{item}</span>
                         </Link>
