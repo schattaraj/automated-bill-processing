383802de1d22e680e639648eec9f472d789c0ee5
sayan-k-d <sayankumar.d2000@gmail.com>
2024-10-23 13:24:32 +0530

    Fixed Claim Reported, folders

--- pages/ClaimReported/index.jsx
+++ pages/ClaimReported/index.jsx
@@ -1,180 +1,187 @@
 import { useState } from "react";
-import ProviderHeader from "../../components/ProviderHeader"
-import ProviderMenu from "../../components/ProviderMenu"
+import ProviderHeader from "../../components/ProviderHeader";
+import ProviderMenu from "../../components/ProviderMenu";
 import ListTable from "@/components/ListTable";
 import CardList from "@/components/CardList";
 import Footer from "@/components/Footer";
 
 function ClaimReported() {
-    const tableColumns = [
-        { "elementName": "patientName", "displayName": "Patient Name" },
-        { "elementName": "age", "displayName": "Age" },
-        { "elementName": "disease", "displayName": "Disease" },
-        { "elementName": "description", "displayName": "Description" },
-        { "elementName": "invoiceNo", "displayName": "Invoice No" },
-        { "elementName": "invoiceAmount", "displayName": "Invoice Amount" },
-        { "elementName": "dateOfAdmit", "displayName": "Date of admit" },
-        { "elementName": "contactNo", "displayName": "Contact No" },
-        { "elementName": "drInCharge", "displayName": "Dr in charge" },
-        { "elementName": "documentStatus", "displayName": "Document Status" },
-        { "elementName": "insuranceCorporates", "displayName": "Insurance Corporates" },
-        { "elementName": "principleMember", "displayName": "Principle Member" }
-    ];
-    const tableData = [
-        {
-            "patientName": "Patrika Mumbi Gacukia",
-            "age": 43,
-            "disease": "Heart",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,000",
-            "dateOfAdmit": "10-03-2005",
-            "contactNo": "9546457511",
-            "drInCharge": "Dr. Preeti Jabbal",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Piya jonn smiths",
-            "age": 24,
-            "disease": "Brain",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,21",
-            "dateOfAdmit": "12-03-2005",
-            "contactNo": "546461454",
-            "drInCharge": "Dr. nila shahanj",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Nil Nithesh kathiwala",
-            "age": 50,
-            "disease": "ortho",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "854611",
-            "dateOfAdmit": "16-03-2005",
-            "contactNo": "87967546",
-            "drInCharge": "Dr. kuldip sing",
-            "documentStatus": "Pending",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Piya jonn smiths",
-            "age": 24,
-            "disease": "Brain",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,000",
-            "dateOfAdmit": "10-03-2005",
-            "contactNo": "9546457511",
-            "drInCharge": "Dr. Preeti Jabbal",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Nil Nithesh kathiwala",
-            "age": 50,
-            "disease": "ortho",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "854611",
-            "dateOfAdmit": "16-03-2005",
-            "contactNo": "87967546",
-            "drInCharge": "Dr. kuldip sing",
-            "documentStatus": "Pending",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Piya jonn smiths",
-            "age": 24,
-            "disease": "Brain",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,000",
-            "dateOfAdmit": "10-03-2005",
-            "contactNo": "9546457511",
-            "drInCharge": "Dr. Preeti Jabbal",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Nil Nithesh kathiwala",
-            "age": 50,
-            "disease": "ortho",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "854611",
-            "dateOfAdmit": "16-03-2005",
-            "contactNo": "87967546",
-            "drInCharge": "Dr. kuldip sing",
-            "documentStatus": "Pending",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Piya jonn smiths",
-            "age": 24,
-            "disease": "Brain",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,21",
-            "dateOfAdmit": "12-03-2005",
-            "contactNo": "546461454",
-            "drInCharge": "Dr. nila shahanj",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Patrika Mumbi Gacukia",
-            "age": 43,
-            "disease": "Heart",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,000",
-            "dateOfAdmit": "10-03-2005",
-            "contactNo": "9546457511",
-            "drInCharge": "Dr. Preeti Jabbal",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        },
-        {
-            "patientName": "Piya jonn smiths",
-            "age": 24,
-            "disease": "Brain",
-            "description": "---",
-            "invoiceNo": "AAR-INV/2023559640",
-            "invoiceAmount": "2,10,000",
-            "dateOfAdmit": "10-03-2005",
-            "contactNo": "9546457511",
-            "drInCharge": "Dr. Preeti Jabbal",
-            "documentStatus": "Proceed",
-            "insuranceCorporates": "Bupa International",
-            "principleMember": "Self"
-        }
-    ];
-    
+  const tableColumns = [
+    { elementName: "patientName", displayName: "Patient Name" },
+    { elementName: "age", displayName: "Age" },
+    { elementName: "disease", displayName: "Disease" },
+    { elementName: "description", displayName: "Description" },
+    { elementName: "invoiceNo", displayName: "Invoice No" },
+    { elementName: "invoiceAmount", displayName: "Invoice Amount" },
+    { elementName: "dateOfAdmit", displayName: "Date of admit" },
+    { elementName: "contactNo", displayName: "Contact No" },
+    { elementName: "drInCharge", displayName: "Dr in charge" },
+    { elementName: "documentStatus", displayName: "Document Status" },
+    { elementName: "insuranceCorporates", displayName: "Insurance Corporates" },
+    { elementName: "principleMember", displayName: "Principle Member" },
+  ];
+  const tableData = {
+    content: [
+      {
+        patientName: "Patrika Mumbi Gacukia",
+        age: 43,
+        disease: "Heart",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,000",
+        dateOfAdmit: "10-03-2005",
+        contactNo: "9546457511",
+        drInCharge: "Dr. Preeti Jabbal",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Piya jonn smiths",
+        age: 24,
+        disease: "Brain",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,21",
+        dateOfAdmit: "12-03-2005",
+        contactNo: "546461454",
+        drInCharge: "Dr. nila shahanj",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Nil Nithesh kathiwala",
+        age: 50,
+        disease: "ortho",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "854611",
+        dateOfAdmit: "16-03-2005",
+        contactNo: "87967546",
+        drInCharge: "Dr. kuldip sing",
+        documentStatus: "Pending",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Piya jonn smiths",
+        age: 24,
+        disease: "Brain",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,000",
+        dateOfAdmit: "10-03-2005",
+        contactNo: "9546457511",
+        drInCharge: "Dr. Preeti Jabbal",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Nil Nithesh kathiwala",
+        age: 50,
+        disease: "ortho",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "854611",
+        dateOfAdmit: "16-03-2005",
+        contactNo: "87967546",
+        drInCharge: "Dr. kuldip sing",
+        documentStatus: "Pending",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Piya jonn smiths",
+        age: 24,
+        disease: "Brain",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,000",
+        dateOfAdmit: "10-03-2005",
+        contactNo: "9546457511",
+        drInCharge: "Dr. Preeti Jabbal",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Nil Nithesh kathiwala",
+        age: 50,
+        disease: "ortho",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "854611",
+        dateOfAdmit: "16-03-2005",
+        contactNo: "87967546",
+        drInCharge: "Dr. kuldip sing",
+        documentStatus: "Pending",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Piya jonn smiths",
+        age: 24,
+        disease: "Brain",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,21",
+        dateOfAdmit: "12-03-2005",
+        contactNo: "546461454",
+        drInCharge: "Dr. nila shahanj",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Patrika Mumbi Gacukia",
+        age: 43,
+        disease: "Heart",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,000",
+        dateOfAdmit: "10-03-2005",
+        contactNo: "9546457511",
+        drInCharge: "Dr. Preeti Jabbal",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+      {
+        patientName: "Piya jonn smiths",
+        age: 24,
+        disease: "Brain",
+        description: "---",
+        invoiceNo: "AAR-INV/2023559640",
+        invoiceAmount: "2,10,000",
+        dateOfAdmit: "10-03-2005",
+        contactNo: "9546457511",
+        drInCharge: "Dr. Preeti Jabbal",
+        documentStatus: "Proceed",
+        insuranceCorporates: "Bupa International",
+        principleMember: "Self",
+      },
+    ],
+  };
+
   return (
     <>
-    <ProviderHeader />
-    <div className="document-list">
+      <ProviderHeader />
+      <div className="document-list">
         <div className="container">
-        <ProviderMenu />
-        <CardList  table={true} heading={"Claim Reported"} tableColumns={tableColumns} tableData={tableData}/>
+          <ProviderMenu />
+          <CardList
+            table={true}
+            heading={"Claim Reported"}
+            tableColumns={tableColumns}
+            tableData={tableData}
+          />
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default ClaimReported
+export default ClaimReported;
--- pages/Folders/index.jsx
+++ pages/Folders/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import ProviderHeader from "../../components/ProviderHeader"
-import ProviderMenu from "../../components/ProviderMenu"
+import ProviderHeader from "../../components/ProviderHeader";
+import ProviderMenu from "../../components/ProviderMenu";
 import ListTable from "@/components/ListTable";
 import CardList from "@/components/CardList";
 import Footer from "@/components/Footer";
@@ -18,171 +18,179 @@ function Folders() {
     "Dr in charge",
     "Document Status",
     "Insurance Corporates",
-    "Principle Member"
-];
-const tableData = [
-  [
-      "Patrika Mumbi Gacukia",
-      43,
-      "Heart",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,000",
-      "10-03-2005",
-      "9546457511",
-      "Dr. Preeti Jabbal",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Piya jonn smiths",
-      24,
-      "Brain",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,21",
-      "12-03-2005",
-      "546461454",
-      "Dr. nila shahanj",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Nil Nithesh kathiwala",
-      50,
-      "ortho",
-      "---",
-      "AAR-INV/2023559640",
-      "854611",
-      "16-03-2005",
-      "87967546",
-      "Dr. kuldip sing",
-      "Pending",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Piya jonn smiths",
-      24,
-      "Brain",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,000",
-      "10-03-2005",
-      "9546457511",
-      "Dr. Preeti Jabbal",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Nil Nithesh kathiwala",
-      50,
-      "ortho",
-      "---",
-      "AAR-INV/2023559640",
-      "854611",
-      "16-03-2005",
-      "87967546",
-      "Dr. kuldip sing",
-      "Pending",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Piya jonn smiths",
-      24,
-      "Brain",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,000",
-      "10-03-2005",
-      "9546457511",
-      "Dr. Preeti Jabbal",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Nil Nithesh kathiwala",
-      50,
-      "ortho",
-      "---",
-      "AAR-INV/2023559640",
-      "854611",
-      "16-03-2005",
-      "87967546",
-      "Dr. kuldip sing",
-      "Pending",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Piya jonn smiths",
-      24,
-      "Brain",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,21",
-      "12-03-2005",
-      "546461454",
-      "Dr. nila shahanj",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Patrika Mumbi Gacukia",
-      43,
-      "Heart",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,000",
-      "10-03-2005",
-      "9546457511",
-      "Dr. Preeti Jabbal",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ],
-  [
-      "Piya jonn smiths",
-      24,
-      "Brain",
-      "---",
-      "AAR-INV/2023559640",
-      "2,10,000",
-      "10-03-2005",
-      "9546457511",
-      "Dr. Preeti Jabbal",
-      "Proceed",
-      "Bupa International",
-      "Self"
-  ]
-]
-const folderList = [
+    "Principle Member",
+  ];
+  const tableData = {
+    content: [
+      [
+        "Patrika Mumbi Gacukia",
+        43,
+        "Heart",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,000",
+        "10-03-2005",
+        "9546457511",
+        "Dr. Preeti Jabbal",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Piya jonn smiths",
+        24,
+        "Brain",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,21",
+        "12-03-2005",
+        "546461454",
+        "Dr. nila shahanj",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Nil Nithesh kathiwala",
+        50,
+        "ortho",
+        "---",
+        "AAR-INV/2023559640",
+        "854611",
+        "16-03-2005",
+        "87967546",
+        "Dr. kuldip sing",
+        "Pending",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Piya jonn smiths",
+        24,
+        "Brain",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,000",
+        "10-03-2005",
+        "9546457511",
+        "Dr. Preeti Jabbal",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Nil Nithesh kathiwala",
+        50,
+        "ortho",
+        "---",
+        "AAR-INV/2023559640",
+        "854611",
+        "16-03-2005",
+        "87967546",
+        "Dr. kuldip sing",
+        "Pending",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Piya jonn smiths",
+        24,
+        "Brain",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,000",
+        "10-03-2005",
+        "9546457511",
+        "Dr. Preeti Jabbal",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Nil Nithesh kathiwala",
+        50,
+        "ortho",
+        "---",
+        "AAR-INV/2023559640",
+        "854611",
+        "16-03-2005",
+        "87967546",
+        "Dr. kuldip sing",
+        "Pending",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Piya jonn smiths",
+        24,
+        "Brain",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,21",
+        "12-03-2005",
+        "546461454",
+        "Dr. nila shahanj",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Patrika Mumbi Gacukia",
+        43,
+        "Heart",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,000",
+        "10-03-2005",
+        "9546457511",
+        "Dr. Preeti Jabbal",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+      [
+        "Piya jonn smiths",
+        24,
+        "Brain",
+        "---",
+        "AAR-INV/2023559640",
+        "2,10,000",
+        "10-03-2005",
+        "9546457511",
+        "Dr. Preeti Jabbal",
+        "Proceed",
+        "Bupa International",
+        "Self",
+      ],
+    ],
+  };
+  const folderList = [
     "Dr Shital Patil",
     "Dr Agh Khan",
     "Dr Nilam Dalvi",
     "Dr Patil Surendra",
     "Dr Preeti Jain",
-    "Dr Kanika Jain"
-]
+    "Dr Kanika Jain",
+  ];
 
   return (
     <>
-    <ProviderHeader />
-    <div className="document-list">
+      <ProviderHeader />
+      <div className="document-list">
         <div className="container">
-        <ProviderMenu />
-        <CardList  table={false} heading={"Folders"} tableColumns={tableColumns} tableData={tableData} folderList={folderList}/>
+          <ProviderMenu />
+          <CardList
+            table={false}
+            heading={"Folders"}
+            tableColumns={tableColumns}
+            tableData={tableData}
+            folderList={folderList}
+          />
         </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Folders
+export default Folders;
